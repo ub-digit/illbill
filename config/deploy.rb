744a457f724c56a75819492816958840a03c1aff
Magnus Kull <magnus.kull@ub.gu.se>
2015-11-13 12:43:30 +0100

    deploy and postgres

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.4.0'
 set :application, 'illbill-server'
 set :repo_url, 'git@github.com:ub-digit/illbill.git'
 
-# set :rvm_ruby_version, '2.2.1'
+set :rvm_ruby_version, '2.1.5'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
